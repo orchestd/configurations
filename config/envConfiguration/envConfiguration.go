cfb678f495e68835b9b8804fae077431511ced33
andrey <andrey.m@heilasystems.com>
2023-09-20 09:57:15 +0300

    added EnablePprofHandlers conf key

--- config/envConfiguration/envConfiguration.go
+++ config/envConfiguration/envConfiguration.go
@@ -1,8 +1,9 @@
 package envConfiguration
 
 type EnvConfiguration struct {
-	DebugMode   bool    `json:"debugMode,omitempty"`
-	HeilaEnv    string  `json:"HEILA_ENV"`
-	ServiceName string  `json:"SERVICE_NAME"`
-	ProjectId   *string `json:"PROJECT_ID,omitempty"`
+	DebugMode           bool    `json:"debugMode,omitempty"`
+	HeilaEnv            string  `json:"HEILA_ENV"`
+	ServiceName         string  `json:"SERVICE_NAME"`
+	ProjectId           *string `json:"PROJECT_ID,omitempty"`
+	EnablePprofHandlers bool    `json:"enablePprofHandlers,omitempty"`
 }
