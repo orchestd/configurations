a54569a1aa60643c1e2de24e72b226fee02a5208
NatanTheBugMaker <Natan.d@heilasystems.com>
2026-04-06 10:37:38 +0300

    POS-11072: NATS_JWT_SIGNATURE_SEED

--- credentials/credentials.go
+++ credentials/credentials.go
@@ -30,7 +30,7 @@ type Credentials struct {
 	NatsJWT  string `envconfig:"NATS_JWT" json:"NATS_JWT"`
 	NatsSeed string `envconfig:"NATS_SEED" json:"NATS_SEED"`
 
-	NatsAccountSigningSeed string `envconfig:"NATS_ACCOUNT_SIGNING_SEED" json:"NATS_ACCOUNT_SIGNING_SEED"`
+	NatsJwtSignatureSeed string `envconfig:"NATS_JWT_SIGNATURE_SEED" json:"NATS_JWT_SIGNATURE_SEED"`
 
 	EncryptKey string `envconfig:"ENCRYPT_KEY" json:"ENCRYPT_KEY"`
 	JwtSecret  string `envconfig:"JWT_SECRET" json:"JWT_SECRET"`
